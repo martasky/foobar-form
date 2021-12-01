fc912df9638f3bc313f60c2fa7f0ad5990a2935c
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-12-02 00:08:28 +0100

    fetch data

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 import "./sass/style.scss";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  loadJSON();
+}
+
+function loadJSON() {
+  let urlBeers = "https://foo-bar-project.herokuapp.com/beertypes";
+  fetch(urlBeers)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      prepareObjects(jsonData);
+    });
+}
+
+function prepareObjects(jsonData) {
+  //modify json here
+  jsonData.forEach((elm) => {
+    console.log(elm);
+    // I thought we will create object like in hogwarts to add beer prices and push the new objects in a global array
+  });
+
+  displayBeer();
+}
+
+function displayBeer() {}
