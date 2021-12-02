da39c731b4170e6064d0e1848ed9385248798bea
Esther <esth2577@stud.kea.dk>
2021-12-02 20:31:42 +0100

    repeated elements adjusted in checkout

--- modules/checkout.js
+++ modules/checkout.js
@@ -2,13 +2,16 @@ import { removeBeer } from "./remove-beer";
 import { calculateTotalPrice } from "./checkout-price";
 import { addBeer } from "./add-beer";
 
-export const selectedBeers = [];
 export function goToCheckOut(allBeers) {
-  //get the information about selected beers
+  let selectedBeers = [];
+  let selectedBeersAmount = []
 
+  document.querySelector(".beers-wrapper").classList.add("hidden");
+  document.querySelector(".checkout-wrapper").classList.remove("hidden");
+  //get the information about selected beers
   let beer_1;
   let beer;
-  
+
   document.querySelectorAll('input[type="checkbox"]').forEach((input) => {
     if (input.checked) {
       beer_1 = input.id.replace("-chosen", "");
@@ -38,22 +41,43 @@ export function goToCheckOut(allBeers) {
   });
 
   //calculate the total price
-  calculateTotalPrice()
+  calculateTotalPrice();
 
   //add event listeners
 
   document.querySelectorAll(".remove_beer").forEach((button) => {
     button.addEventListener("click", () => {
-        removeBeer(button)
+      removeBeer(button);
     });
   });
 
-  document.querySelectorAll(".add_beer").forEach((button) =>{
+  document.querySelectorAll(".add_beer").forEach((button) => {
     button.addEventListener("click", () => {
-        addBeer(button)
+      addBeer(button);
     });
-  })
+  });
 
-  document.querySelector(".beers-wrapper").classList.add("hidden");
-  document.querySelector(".checkout-wrapper").classList.remove("hidden");
+  // if you proceed send the selectedBeers array
+  document.querySelector("#checkout_next").addEventListener("click", () => {
+    console.log("next");
+    document.querySelector(".checkout-wrapper").classList.add("hidden");
+    //loop throught the added beers, build an object with info and push it to the array selectedBeersAmount
+    //document.querySelector("").classList.remove("hidden");
+    //displayOrderSummary(selectedBeers)
+  });
+
+  // if you go back clean the selectedBeers array and the html
+
+  document.querySelector("#checkout_back").addEventListener("click", () => {
+    console.log("backwe go");
+    document.querySelector(".checkout-wrapper").classList.add("hidden");
+    document.querySelector(".beers-wrapper").classList.remove("hidden");
+    document.querySelector(".beers_ordered").innerHTML = "";
+    document.querySelector(".total_price span").textContent = "0"
+    selectedBeers = [];
+    selectedBeersAmount = []
+    
+  });
+
+
 }
