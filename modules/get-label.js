e21f1202d4e2465a0daa5aade9e205b899ee45e5
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-12-13 17:41:32 +0100

    change url

--- modules/get-label.js
+++ modules/get-label.js
@@ -1,6 +1,6 @@
 export function getLabel(elm) {
   let label = elm.label.replace("png", "webp");
 
-  let imgUrl = "/assets/beer-images/" + label;
+  let imgUrl = new URL("/assets/beer-images/" + label, import.meta.url);
   return imgUrl;
 }
