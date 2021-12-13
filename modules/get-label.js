97749294b237d5b2d70e9b5687cf07f98fb3d6e6
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-12-13 17:43:38 +0100

    correct url

--- modules/get-label.js
+++ modules/get-label.js
@@ -1,6 +1,6 @@
 export function getLabel(elm) {
   let label = elm.label.replace("png", "webp");
 
-  let imgUrl = new URL("/assets/beer-images/" + label, import.meta.url);
+  let imgUrl = new URL("/assets/beer-images/" + label, import.meta.url).href;
   return imgUrl;
 }
